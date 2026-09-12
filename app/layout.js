97f9f416f42b9adebf86e4952ba554ec485310e2
sahil <sahilsid2001@gmail.com>
2026-09-12 15:20:22 +0530

    fix: update script source to production URL for consent handling

--- app/layout.js
+++ app/layout.js
@@ -27,8 +27,8 @@ export default function RootLayout({ children }) {
       <head>
         <script
           async
-          // src="https://cdn.digitalanumati.com/uat/v1/anumati-dpdp-consent-v1.js"
-          src="http://localhost:4173/anumati-dpdp-consent-v1.js"
+          src="https://cdn.digitalanumati.com/uat/v1/anumati-dpdp-consent-v1.js"
+          // src="http://localhost:4173/anumati-dpdp-consent-v1.js"
           data-site-key="APP_httpsbright-school-webvercelapp_1789037524790"
           application-id="d522836a-b10d-42e6-a9e0-35a69ba030d4"
         ></script>
